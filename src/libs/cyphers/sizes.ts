02a5b0e9c1c70688579e2cfe3218e82fa88a4968
naram.dash <juho_kim@outlook.com>
2021-10-12 22:17:53 +0900

    .

--- src/libs/cyphers/sizes.ts
+++ src/libs/cyphers/sizes.ts
@@ -1,42 +1,89 @@
-const Resolutions = []
+type NicknameSection = {
+  x: number
+  y: number
+  w: number
+  h: number
+}
+
+type NicknameSectionsOnLoad = {
+  friendly: [
+    NicknameSection,
+    NicknameSection,
+    NicknameSection,
+    NicknameSection,
+    NicknameSection,
+  ]
+  enemy: [
+    NicknameSection,
+    NicknameSection,
+    NicknameSection,
+    NicknameSection,
+    NicknameSection,
+  ]
+}
 
 type Detail = {
-  nicknameSectionsOnLoad: {
-    x: number
-    y: number
-    w: number
-    h: number
-  }[]
+  nicknameSectionsOnLoad: NicknameSectionsOnLoad
 }
 const Size: Record<string, Detail> = {
-  '1024*768': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1152*864': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1280*960': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1280*1024': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1366*768': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1440*900': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1600*900': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1680*1050': {
-    nicknameSectionsOnLoad: [],
-  },
-  '1920*1080': {
-    nicknameSectionsOnLoad: [],
-  },
-  '2560*1080': {
-    nicknameSectionsOnLoad: [],
-  },
+  // '1024*768': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1152*864': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1280*960': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1280*1024': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1366*768': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1440*900': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1600*900': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1680*1050': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '1920*1080': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
+  // '2560*1080': {
+  //   nicknameSectionsOnLoad: {
+  //     enemy: [],
+  //     friendly: [],
+  //   },
+  // },
 }
--- src/libs/tesseract.ts
+++ src/libs/tesseract.ts
@@ -2,17 +2,11 @@ import Tesseract from 'tesseract.js'
 
 async function ocr(canvasElem: HTMLCanvasElement) {
   const { data } = await Tesseract.recognize(canvasElem, 'kor+eng', {
-<<<<<<< HEAD
     workerPath:
       'https://unpkg.com/tesseract.js@v2.0.0/dist/worker.min.js',
     langPath: 'https://tessdata.projectnaptha.com/4.0.0',
     corePath:
       'https://unpkg.com/tesseract.js-core@v2.0.0/tesseract-core.wasm.js',
-=======
-    workerPath: 'https://unpkg.com/tesseract.js@v2.0.0/dist/worker.min.js',
-    langPath: 'https://tessdata.projectnaptha.com/4.0.0',
-    corePath: 'https://unpkg.com/tesseract.js-core@v2.0.0/tesseract-core.wasm.js',
->>>>>>> 162d1a36bbc17b3388b5e3862f93bdaa63728312
 
     logger: (m) => console.log(m),
   })
